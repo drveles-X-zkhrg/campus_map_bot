f9c60dbaa6dfaf773b1be905fc7b73879494d299
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-05-17 20:17:34 +0300

    trying auth with requests

--- auth_webdriver.py
+++ auth_webdriver.py
@@ -1,31 +1,41 @@
+import time, os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from time import sleep
 from dotenv import load_dotenv, dotenv_values
-import os
 
 
 def loging_dotenv():
+    '''
+    Gettin login and pass from .env file to edu.21-school.ru
+    
+    File format: 
+        LOGIN={login}
+        PASSWORD={pass}
+    '''
+
     dotenv_path = os.path.join(".env")
     load_dotenv(dotenv_path)
 
 
 def main():
+    loging_dotenv()
+    user_login = dotenv_values(".env").get("LOGIN")
+    user_pass = dotenv_values(".env").get("PASSWORD")
+
     driver = webdriver.Chrome()
     driver.get("https://edu.21-school.ru/")
-    loging_dotenv()
     login_field = driver.find_element(By.NAME, "username")
-    login_field.send_keys(dotenv_values(".env").get("LOGIN"))
+    login_field.send_keys(user_login)
     pasword_field = driver.find_element(By.NAME, "password")
-    pasword_field.send_keys(dotenv_values(".env").get("PASSWORD"))
+    pasword_field.send_keys(user_pass)
     driver.find_element(By.XPATH, '//*[@id="login"]/div/div/div[2]/div/div/form/div[3]/button').click()
     driver.get("https://edu.21-school.ru/campus") 
 
 
     print(login_field)
 
-    sleep(10)
+    time.sleep(10)
     
 
 
--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-import requests
-import pprint
-import time
+'''
+This is entry point to autification functions.
+'''
 
-
-def test_requests():
-    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-    payload = {"username": "jenniffr@student.21-school.ru", "password": "b"}
-    headers = {
-        "User-Agent": user_agent,
-    }
-    with requests.Session() as session:
-        response = session.get("https://edu.21-school.ru/", headers=headers, auth=("jenniffr@student.21-school.ru", "a"))
-
-        print(response.url)
-        print(response.status_code)
-        response = session.get("https://edu.21-school.ru/")
-        print(response.url)
-        print(response.status_code)
-
-
-def main():
-    test_requests()
-
-
-if __name__ == "__main__":
-    main()
