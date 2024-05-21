49d4d985513af31a4f2c81d1fbd1d834610c400d
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-05-21 16:35:27 +0300

    testing ram usage on vds

--- auth_and_parse_edu.py
+++ auth_and_parse_edu.py
@@ -73,6 +73,8 @@ def login_and_parse_campus_map():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument('window-size=1200x1040')   
     driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
