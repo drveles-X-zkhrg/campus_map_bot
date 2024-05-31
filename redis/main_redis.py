2fd120d6f853589878a4c9a16240ffb637c664ff
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-05-31 18:46:15 +0300

    only parser here

--- redis/main_redis.py
+++
@@ -1,8 +0,0 @@
-from initialize_redis import *
-
-if __name__ == "__main__":
-    initialize_redis()
-    client = redis.Redis(host="localhost", port=6379, db=0)
-    client.hgetall("tg_id")
-    client.hgetall("peer_nick")
-   
