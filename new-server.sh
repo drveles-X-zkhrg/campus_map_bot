34098846d016cdf767884eeb448277e3984bea8e
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-06-04 20:14:38 +0300

    deploed but not work

--- new-server.sh
+++ new-server.sh
@@ -5,6 +5,10 @@ wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add - \
 && apt-get install -y google-chrome-stable \
 && rm -rf /var/lib/apt/lists/*
 
+curl -O https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.141/linux64/chromedriver-linux64.zip
+unzip chromedriver-linux64.zip
+cp chromedriver-linux64/chromedriver ./parser
+
 python3 -m venv venv
 . venv/bin/activate
 pip install -r ./api/requirements.txt 
@@ -12,14 +16,14 @@ pip install -r ./frontend/requirements.txt
 pip install -r ./parser/requirements.txt 
 pip install -r ./redis/requirements.txt 
 
-curl -O https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.141/linux64/chromedriver-linux64.zip
-unzip chromedriver-linux64.zip
-cp chromedriver-linux64/chromedriver ./parser
-
+. venv/bin/activate
 set -a # automatically export all variables
 source .env
 set +a
 
-
 python3 ./parser/main.py > parser.log &
-redis-server --daemonize yes
+redis-server --daemonize yes 
+python3 ./frontend/main.py > frontend.log &
+cd api
+uvicorn app.main:app --host localhost --port 8000 > ../api.log & 
+cd ..
