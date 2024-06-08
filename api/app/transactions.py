ce538a3458574c090b62e196ce1a1041f6339da6
Zakhar Griaznov <zakhar.griaznov@gmail.com>
2024-06-09 01:44:07 +0300

    develop

--- api/app/transactions.py
+++ api/app/transactions.py
@@ -2,8 +2,12 @@ import redis
 from typing import Dict
 
 
-def transa(r: redis.StrictRedis, set_name: str, h: Dict):
+def update_peers(r: redis.StrictRedis, set_name: str, h: Dict):
     pipe = r.pipeline()
+    old_peers = pipe.smembers(set_name)
+    for old_peer in old_peers:
+        pipe.hset(old_peer, "status", "0")
+
     for el in h:
         pipe.sadd(set_name, el)
         pipe.hmset(el, h[el])
