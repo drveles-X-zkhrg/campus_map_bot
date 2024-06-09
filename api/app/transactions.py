84fc431c328bde66ab7df6fd5b93a5161785f46b
Zakhar Griaznov <zakhar.griaznov@gmail.com>
2024-06-09 19:00:46 +0300

    fix pipeline set rediska

--- api/app/transactions.py
+++ api/app/transactions.py
@@ -3,13 +3,13 @@ from typing import Dict
 
 
 def update_peers(r: redis.StrictRedis, set_name: str, h: Dict):
+    old_peers = r.smembers(set_name)
     pipe = r.pipeline()
-    old_peers = pipe.smembers(set_name)
     for old_peer in old_peers:
         pipe.hset(old_peer, "status", "0")
 
     for el in h:
         pipe.sadd(set_name, el)
-        pipe.hmset(el, h[el])
+        pipe.hset(el, mapping=h[el])
 
     pipe.execute()
