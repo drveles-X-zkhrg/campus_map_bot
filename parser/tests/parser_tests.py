8b77bcd3de46a9397a595d0da5176e6eec848dfb
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-08-24 14:37:33 +0300

    style: pylinted

--- parser/tests/parser_tests.py
+++ parser/tests/parser_tests.py
@@ -2,21 +2,13 @@
 ## Tests for parser service
 """
 
-import logging
-from parse_edu import login_and_parse_campus_map
-from parse_raw_from_html import convert_to_json
-from parser_sender import update_peers
 
-def test_conn_parser_to_api():
+def parse_via_api():
     """
     Test connecion to API service
     """
-    test_data = {("jenniff", "va", "f", 3)}
-    logging.info("test data to convert: %s", test_data)
-    test_json = convert_to_json(test_data)
-    logging.info("converted to test json: %s", test_json)
-    update_peers(test_json)
-    logging.info("sended to api ?")
+    return
+
 
 if __name__ == "__main__":
-    test_conn_parser_to_api()
+    parse_via_api()
