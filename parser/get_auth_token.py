ffe0b71bb6103fbf3b6b3abb83c25786085c3f96
Egor Petrov <egor.petrov@imms.de>
2026-01-26 12:25:23 +0300

    Добавил кэширования для токена

--- parser/get_auth_token.py
+++ parser/get_auth_token.py
@@ -4,13 +4,23 @@
 
 import logging
 import os
+import time
 import requests
+
+_cached_token = None
+_token_expiry = 0
 
 
 def get_token():
     """
     ###  Auth and return token: `Bearer $token`
     """
+
+    global _cached_token, _token_expiry
+
+    if _cached_token and time.time() < _token_expiry:
+        return _cached_token
+
     request = requests.post(
         url="https://auth.21-school.ru/auth/realms/EduPowerKeycloak/protocol/openid-connect/token",
         data={
@@ -27,6 +37,20 @@ def get_token():
             "Failed to getting token! %d Error: %s", request.status_code, request.json()
         )
 
-    token = "Bearer " + request.json().get("access_token", "")
+    token_response = request.json()
+    access_token = token_response.get("access_token", "")
 
-    return token
+    _cached_token = "Bearer " + access_token
+
+    expires_in = token_response.get("expires_in", 3600)
+    _token_expiry = time.time() + expires_in - 60
+
+    return _cached_token
+
+
+def clear_token_cache():
+    """Clean cache"""
+
+    global _cached_token, _token_expiry
+    _cached_token = None
+    _token_expiry = 0
